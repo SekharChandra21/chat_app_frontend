e5145ad7740974d2a132f78083f90317c6c4c004
SekharChandra21 <sekhararipaka46@gmail.com>
2026-03-14 23:46:20 +0530

    axios updated

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,10 +1,21 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL : import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "https://chatappbackend-production-65b7.up.railway.app/api",
+    baseURL : "https://chatappbackend-production-65b7.up.railway.app/api",
+    // baseURL : import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "https://chatappbackend-production-65b7.up.railway.app/api",
     withCredentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     headers: {
-        "Access-Control-Allow-Origin" : "*",
         "Content-Type": "application/json",},
 })
+
+// Add interceptor to attach token
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("Jwt_Token"); // or wherever you store it
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
